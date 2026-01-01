848c2d38ee7b6fb7510747eb6b169b6f0785e3ef
Ayush Umrao <umraoayush@gmail.com>
2026-01-02 00:31:04 +0530

    Refactor folder name: basic_widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgetsflutter/basic_widgets.dart/column_widget/column_widget.dart';
-
-
-
-
-
-
-
-
+import 'package:widgetsflutter/basic_widgets/column_widget/column_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-       home: const ColumnWidget());
+      home: const ColumnWidget(),
+    );
   }
 }
